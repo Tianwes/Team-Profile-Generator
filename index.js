46044865a4f1d0e36042c80b0a84f3cb355ccc88
Tianwes <willnotsuffice@gmail.com>
2021-02-27 17:39:50 -0500

    Got heading to show up in generated html, need to pass in employee info, fix tests

--- index.js
+++ index.js
@@ -2,101 +2,106 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
 const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+
+// const outPutFolder = path.resolve(__dirname, "output");
+// const htmlFile = path.join(outPutFolder, "team.html");
+// const writeCard = require("./template");
+
 const team = [];
-const outPutFolder = path.resolve(__dirname, "output");
-const htmlFile = path.join(outPutFolder, "team.html");
-const writeCard = require("./src/template")
-const generateHTML = (answers) => {
-`
-<!DOCTYPE html>
-<html lang="en">
+// const generateHTML = (answers) => {
+//     `
+// <!DOCTYPE html>
+// <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <title>Document</title>
-</head>
-<body>
-    <div class="container mw-100 p-3 shadow bg-danger text-white text-center display-4 ">My Team</div>
-    <div class="container">
-    <div class="row text-center my-4 justify-content-center">
-        <div class="col-lg-4 col-md-6">
-            <div class="card text-center shadow">
-                <div class="card-body p-0">
-                    <div class="card-header bg-primary">
-                        <h4 class="card-title">${answers.managersName}</h4>
-                        <h5 class="card-title">icon + Manager</h5>
-                    </div>
-                    <button type="button" class="btn btn-light border w-75 mt-3">ID: ${answers.managersID}</button>
-                    <button type="button" class="btn btn-light border w-75">Email: ${answers.managersEmail}</button>
-                    <button type="button" class="btn btn-light border w-75 mb-3">Phone: ${answers.managersPhone}</button>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-md-6">
-            <div class="card text-center shadow">
-                <div class="card-body p-0">
-                    <div class="card-header bg-primary">
-                        <h4 class="card-title">${answers.employee1name}</h4>
-                        <h5 class="card-title">icon + Manager</h5>
-                    </div>
-                    <button type="button" class="btn btn-light border w-75 mt-3">ID</button>
-                    <button type="button" class="btn btn-light border w-75">EMAIL</button>
-                    <button type="button" class="btn btn-light border w-75 mb-3">OFFICE #</button>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-md-6">
-            <div class="card text-center shadow">
-                <div class="card-body p-0">
-                    <div class="card-header bg-primary">
-                        <h4 class="card-title">Manager's Name</h4>
-                        <h5 class="card-title">icon + Manager</h5>
-                    </div>
-                    <button type="button" class="btn btn-light border w-75 mt-3">ID</button>
-                    <button type="button" class="btn btn-light border w-75">EMAIL</button>
-                    <button type="button" class="btn btn-light border w-75 mb-3">OFFICE #</button>
-          </div>
-            </div>      
-        </div>
-    </div>
-    <div class="row text-center my-4 justify-content-center">
-        <div class="col-lg-4 col-md-6">
-            <div class="card text-center shadow">
-                <div class="card-body p-0">
-                    <div class="card-header bg-primary">
-                        <h4 class="card-title">Manager's Name</h4>
-                        <h5 class="card-title">icon + Manager</h5>
-                    </div>
-                    <button type="button" class="btn btn-light border w-75 mt-3">ID</button>
-                    <button type="button" class="btn btn-light border w-75">EMAIL</button>
-                    <button type="button" class="btn btn-light border w-75 mb-3">OFFICE #</button>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-md-6">
-            <div class="card text-center shadow">
-                <div class="card-body p-0">
-                    <div class="card-header bg-primary">
-                        <h4 class="card-title">Manager's Name</h4>
-                        <h5 class="card-title">icon + Manager</h5>
-                    </div>
-                    <button type="button" class="btn btn-light border w-75 mt-3">ID</button>
-                    <button type="button" class="btn btn-light border w-75">EMAIL</button>
-                    <button type="button" class="btn btn-light border w-75 mb-3">OFFICE #</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<script type="text/javascript" src="index.js"></script>
-</body>
-</html>
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <div class="container mw-100 p-3 shadow bg-danger text-white text-center display-4 ">My Team</div>
+//     <div class="container">
+//     <div class="row text-center my-4 justify-content-center">
+//         <div class="col-lg-4 col-md-6">
+//             <div class="card text-center shadow">
+//                 <div class="card-body p-0">
+//                     <div class="card-header bg-primary">
+//                         <h4 class="card-title">${answers.managersName}</h4>
+//                         <h5 class="card-title">icon + Manager</h5>
+//                     </div>
+//                     <button type="button" class="btn btn-light border w-75 mt-3">ID: ${answers.managersID}</button>
+//                     <button type="button" class="btn btn-light border w-75">Email: ${answers.managersEmail}</button>
+//                     <button type="button" class="btn btn-light border w-75 mb-3">Phone: ${answers.managersPhone}</button>
+//                 </div>
+//             </div>
+//         </div>
+//         <div class="col-lg-4 col-md-6">
+//             <div class="card text-center shadow">
+//                 <div class="card-body p-0">
+//                     <div class="card-header bg-primary">
+//                         <h4 class="card-title">${answers.employee1name}</h4>
+//                         <h5 class="card-title">icon + Manager</h5>
+//                     </div>
+//                     <button type="button" class="btn btn-light border w-75 mt-3">ID</button>
+//                     <button type="button" class="btn btn-light border w-75">EMAIL</button>
+//                     <button type="button" class="btn btn-light border w-75 mb-3">OFFICE #</button>
+//                 </div>
+//             </div>
+//         </div>
+//         <div class="col-lg-4 col-md-6">
+//             <div class="card text-center shadow">
+//                 <div class="card-body p-0">
+//                     <div class="card-header bg-primary">
+//                         <h4 class="card-title">Manager's Name</h4>
+//                         <h5 class="card-title">icon + Manager</h5>
+//                     </div>
+//                     <button type="button" class="btn btn-light border w-75 mt-3">ID</button>
+//                     <button type="button" class="btn btn-light border w-75">EMAIL</button>
+//                     <button type="button" class="btn btn-light border w-75 mb-3">OFFICE #</button>
+//           </div>
+//             </div>      
+//         </div>
+//     </div>
+//     <div class="row text-center my-4 justify-content-center">
+//         <div class="col-lg-4 col-md-6">
+//             <div class="card text-center shadow">
+//                 <div class="card-body p-0">
+//                     <div class="card-header bg-primary">
+//                         <h4 class="card-title">Manager's Name</h4>
+//                         <h5 class="card-title">icon + Manager</h5>
+//                     </div>
+//                     <button type="button" class="btn btn-light border w-75 mt-3">ID</button>
+//                     <button type="button" class="btn btn-light border w-75">EMAIL</button>
+//                     <button type="button" class="btn btn-light border w-75 mb-3">OFFICE #</button>
+//                 </div>
+//             </div>
+//         </div>
+//         <div class="col-lg-4 col-md-6">
+//             <div class="card text-center shadow">
+//                 <div class="card-body p-0">
+//                     <div class="card-header bg-primary">
+//                         <h4 class="card-title">Manager's Name</h4>
+//                         <h5 class="card-title">icon + Manager</h5>
+//                     </div>
+//                     <button type="button" class="btn btn-light border w-75 mt-3">ID</button>
+//                     <button type="button" class="btn btn-light border w-75">EMAIL</button>
+//                     <button type="button" class="btn btn-light border w-75 mb-3">OFFICE #</button>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+// </div>
+// <script type="text/javascript" src="index.js"></script>
+// </body>
+// </html>
 
-`
-};
+// `
+// };
 
 const askManager = () => {
     inquirer.prompt([
@@ -119,10 +124,11 @@ const askManager = () => {
             type: "input",
             message: "Manager's office phone number",
             name: "managersPhone"
-        }]).then(answers => {
-            console.log(answers);
+        }]).then((answers) => {
+            // New manager obj is created
             const manager = new Manager(answers.managersName, answers.managersID, answers.managersEmail, answers.managersPhone);
             console.log(manager);
+            // Push new manager obj to team array
             team.push(manager);
             mainMenu();
         })
@@ -150,6 +156,8 @@ const askEngineer = () => {
             name: "EngineersGithub"
         }]).then(answers => {
             console.log(answers);
+            const engineer = new Engineer(answers.EngineersName, answers.EngineersID, answers.EngineersEmail, answers.EngineersGithub);
+            team.push(engineer);
             mainMenu();
         })
 }
@@ -172,66 +180,65 @@ const askIntern = () => {
         },
         {
             type: "input",
-            message: "Intern's GitHub page",
-            name: "InternsGithub"
+            message: "Intern's schhol name",
+            name: "InternsSchool"
         }]).then(answers => {
-            console.log(answers);
+            const intern = new Intern(answers.InternsName, answers.InternsID, answers.InternsEmail, answers.InternsSchool);
+            team.push(intern);
             mainMenu();
         })
 }
+
 askManager();
 
-const mainMenu = () => {
-    inquirer.prompt ([
+function mainMenu() {
+
+    inquirer.prompt([
         {
             type: "list",
             message: "Select a team-member role for next team-member card",
-            choices:["Engineer", "Intern", "Finished building team"],
-            name:"nextEmployeeRole",
-          
+            choices: ["Engineer", "Intern", "Finished building team"],
+            name: "nextEmployeeRole",
         }
     ]).then(answers => {
-        if(answers.nextEmployeeRole === "Engineer"){
+        if (answers.nextEmployeeRole === "Engineer") {
             askEngineer();
         }
-        if(answers.nextEmployeeRole === "Intern"){
+        if (answers.nextEmployeeRole === "Intern") {
             askIntern();
         }
-        if(answers.nextEmployeeRole === "Finished building team"){
+        if (answers.nextEmployeeRole === "Finished building team") {
             buildTeam();
         }
-    })
+    }).then("help");
 }
-    function buildTeam(){
-        if(!fs.existsSync(outPutFolder)){
-            fs.mkdirSync(outPutFolder)
-        }
-        fs.writeFile(htmlFile, writeCard(team), "utf-8")
-    }
-  
-    // {
-    //     type: "input",
-    //     message: "Manager's name",
-    //     name: "managersName"
-    // },
-    // {
-    //     type: "input",
-    //     message: "Manager's name",
-    //     name: "managersName"
-    // },
-    // {
-    //     type: "input",
-    //     message: "Manager's name",
-    //     name: "managersName"
-    // },
-// ]).then((answers) => {
-    // function() {
 
-    // }
+function buildTeam() {
 
-//     const htmlContent = generateHTML(answers);
+    const html =
+        `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <title>Document</title>
+    </head>
+    <body>
 
-//     fs.writeFile("index.html", htmlContent, (err) => {
-//         err ? console.error(err) : console.log("Your html page has been written :)")
-//     })
-// })
+    <script type="text/javascript" src="index.js"></script>
+    </body>
+
+    </html>
+    `
+    const testing = require("./src/template");
+
+
+    fs.writeFile("index.html", html + testing, (err) => {
+        err ? console.error(err) : console.log("Your README has been written :)")
+    })
+
+}
+
+
--- src/template.js
+++ src/template.js
@@ -1,36 +1,33 @@
+const Employee = require("../lib/Employee")
+
 const generateTeam = (team) => {
     const generateManager = (manager) => {
         return `
         <div class="card text-center shadow">
         <div class="card-body p-0">
-            <div clasrs="card-header bg-primary">
-                <h4 class="card-title">${answers.managersName}</h4>
+            <div class="card-header bg-primary">
+                <h4 class="card-title">${manager.managersName}</h4>
                 <h5 class="card-title">icon + Manager</h5>
             </div>
-            <button type="button" class="btn btn-light border w-75 mt-3">ID: ${answers.managersID}</button>
-            <button type="button" class="btn btn-light border w-75">Email: ${answers.managersEmail}</button>
-            <button type="button" class="btn btn-light border w-75 mb-3">Phone: ${answers.managersPhone}</button>
+            <button type="button" class="btn btn-light border w-75 mt-3">ID: ${manager.managersID}</button>
+            <button type="button" class="btn btn-light border w-75">Email: ${manager.managersEmail}</button>
+            <button type="button" class="btn btn-light border w-75 mb-3">Phone: ${manager.managersPhone}</button>
         </div>
     </div>
-</div>`
+ `
 
     }
     const html = [];
-    html.push(team.filter(employee => employee.getRole() === "Manager")).map(manager => generateManager(manager));
-}
-module.exports = (team) => {
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
+    let certMember = team.filter(employee => employee.getRole() === "Manager")
+    html.push(certMember)
+    console.log(html)
+    // html.push(team.filter(employee => employee.getRole() === "Manager")).map(manager => generateManager(manager));
+    // const generateEngineer = ()
     
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        <title>Document</title>
-    </head>
-    <body>
+}
+
+module.exports = team => {
+    return `
         <div class="container mw-100 p-3 shadow bg-danger text-white text-center display-4 ">My Team</div>
         <div class="container">
         <div class="row text-center my-4 justify-content-center">
@@ -38,9 +35,6 @@ module.exports = (team) => {
             ${generateTeam(team)}
             </div>
         </div>
-        </div>
-        <script type="text/javascript" src="index.js"></script>
-    </body>
-    </html>   
+        </div>  
 `
-}
+};
